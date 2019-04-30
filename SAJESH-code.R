f44c3c5182deb0adf1db5129c981f06bd5cad077
Sajesh Bahing <sajesh.bahing22@gmail.com>
2019-04-30 14:16:30 +1000

    task 3 and 4

--- SAJESH-code.R
+++ SAJESH-code.R
@@ -1,10 +1,8 @@
-source(file = "./AggWaFit718.R")
-
 # Task 1.ii -> reading the data as a table and converting to matrix using matrix function
 data = as.matrix(read.table('./Energy19.txt'))
 
 # Task 1.iii -> subset of 300 data from all the data provided
-subsetData = data[sample(1:671, 300),c(1:6)]
+subsetData = data[1:300,]
 
 # Task 1.iv -> Scatter plots and histograms
 plot(subsetData[,6],  subsetData[,1], type = "p", ylab = "Energy use of appliances in Wh", xlab = "Temperature in kitchen (Celcius)", col = "blue", pch = 4 )
@@ -36,16 +34,44 @@ scaleData = function (temp) {
 }
 
 selectedData = subsetData[,-5]
-
-normalizedData = log(scaleData(selectedData) + 1)
+selectedData = log(selectedData + 1)
+normalizedData = apply(selectedData, 2, scaleData)
+summary(normalizedData)
 
 write.table(normalizedData, "SAJESH-transformed.txt", col.names = FALSE, append = FALSE, sep=",", row.names = FALSE)
 
-# Task 3.i -> Models
+# Task 3.i 
 #
+source(file = "./AggWaFit718.R")
+
+#Task 3.ii  -> Models
 # output directory is required in the project directory
 fit.QAM(normalizedData, "./output/output.txt", "./output/status.txt" )
 fit.QAM(normalizedData, "./output/output1.txt", "./output/status1.txt", g=PM05, g.inv = invPM05)
 fit.QAM(normalizedData, "./output/output2.txt", "./output/status2.txt", g=QM, g.inv = invQM)
 fit.OWA(normalizedData, "./output/output3.txt", "./output/status3.txt")
 fit.choquet(normalizedData, "./output/output4.txt", "./output/status4.txt")
+
+#Task 4.i
+input = scaleData(sqrt(c(18,44,4,74.8)))
+weights = c(0.550409014516637,0.214286745589743,0.120432080374135,0.114872159519482)
+
+cweights = c(0
+             ,0
+             ,0
+             ,0.193637956140186
+             ,0.403822529954561
+             ,0.279041769700155
+             ,0.725165889386119
+             ,0.0212347951826433
+             ,0.0212347951826433
+             ,0.0212347951826433
+             ,0.837194766749031
+             ,0.420888145066684
+             ,0.745010462552994
+             ,0.420888145066693
+             ,0.999999999999913)
+
+QAM(input, weights)
+
+choquet(input, cweights)
